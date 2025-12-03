3ba483f31e63e02c57f30d456d1c39e39ff40551
josecoronel20 <josecoronel20@outlook.com>
2025-12-03 12:11:11 -0300

    add google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -122,6 +123,19 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${inter.className} ${montserrat.variable}`}>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11151875862"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11151875862');
+          `}
+        </Script>
         <StructuredData type="Organization" data={organizationData} />
         <Header />
         <main className="min-h-screen">{children}</main>
